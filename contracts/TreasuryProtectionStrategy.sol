175d5121d9b2a7d0ad4c555eba40a2a388d10008
Ignacio Freire <nachofreire@gmail.com>
2021-11-24 12:15:15 -0300

    Removed indexed array from WhitelistAddresses event and added test case

--- contracts/TreasuryProtectionStrategy.sol
+++ contracts/TreasuryProtectionStrategy.sol
@@ -14,7 +14,7 @@ contract TreasuryProtectionStrategy is StrategyBase {
         mapping(address => Whitelist) protection; 
     }
 
-    event WhitelistAddresses(address[] indexed newWhitelistAddresses);
+    event WhitelistAddresses(address[] whitelist);
 
     constructor(Guardian _guardian, LosslessController _controller) StrategyBase(_guardian, _controller) {}
 
@@ -36,8 +36,8 @@ contract TreasuryProtectionStrategy is StrategyBase {
         for(uint8 i = 0; i < whitelist.length; i++) {
             protectedAddresses[token].protection[protectedAddress].whitelist[whitelist[i]] = true;
         }
-        guardian.setProtectedAddress(token, protectedAddress);
         emit WhitelistAddresses(whitelist);
+        guardian.setProtectedAddress(token, protectedAddress);
     }
 
     // @dev Remove whitelist for protected addresss.
